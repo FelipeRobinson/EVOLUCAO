fff3a47145fc29ea794250dbceb2fe1ec758c56f
Felipe Spider <feliperobinsonprofissional@gmail.com>
2026-04-20 16:26:48 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener("scroll", function () {
+    const nav = document.getElementById("navbar");
+    if (window.scrollY > window.innerHeight * 0.8) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
+const hamburguer = document.getElementById("hamburguer");
+const menu = document.getElementById("menu");
+
+hamburguer.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    hamburguer.classList.toggle("active");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const numeros = [
+        "5519989132598",
+        "5519992947773"
+    ];
+
+    const botoes = document.querySelectorAll(".whatsapp-btn");
+
+    botoes.forEach(btn => {
+        btn.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const numeroEscolhido = numeros[Math.floor(Math.random() * numeros.length)];
+
+            const mensagem = "Olá, gostaria de mais informações sobre adquirir a minha CNH!";
+            const url = `https://wa.me/${numeroEscolhido}?text=${encodeURIComponent(mensagem)}`;
+
+            window.open(url, "_blank");
+        });
+    });
+});
